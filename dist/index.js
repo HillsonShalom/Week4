a8740ffc0b367e7610e7d208172ec485b81af2a9
BennyNoyman <bn0504127633@gmail.com>
2024-09-26 17:57:59 +0300

    cors

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const beeperController_1 = __importDefault(require("./controllers/beeperController"));
 // load enviroment variables
 require("dotenv/config");
 const app = (0, express_1.default)();
+const corsOps = {
+    origin: `http://localhost:${process.env.PORT}`
+};
 app.use(express_1.default.json());
-app.use('/api/beepers', beeperController_1.default);
+app.use('/api/beepers', (0, cors_1.default)(corsOps), beeperController_1.default);
 app.listen(process.env.PORT, () => console.log(`See you at http::localhost:${process.env.PORT}`));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import exp, { Express } from 'express';
+import cors from 'cors'
 import beeperController from './controllers/beeperController'
 
 // load enviroment variables
@@ -6,8 +7,11 @@ import 'dotenv/config';
 
 const app: Express = exp();
 
+const corsOps = {
+    origin: `http://localhost:${process.env.PORT}`
+}
 app.use(exp.json());
-app.use('/api/beepers', beeperController);
+app.use('/api/beepers', cors(corsOps), beeperController);
 
 app.listen(process.env.PORT, ():void => console.log(`See you at http::localhost:${process.env.PORT}`));
 
